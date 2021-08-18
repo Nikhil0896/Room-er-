7485ea0194ef3344cb0c660aefcb27902d75ca68
Nikhil0896 <nikhil.kulkarni.rajendra@gmail.com>
2021-08-18 20:15:11 -0230

    Changes userdetails

--- src/main/java/com/springboot/spring/config/UserDetailsServiceImpl.java
+++ src/main/java/com/springboot/spring/config/UserDetailsServiceImpl.java
@@ -39,6 +39,7 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 		
 		
 		return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), grantedAuthorities);
+		
 	}
 	
 	
